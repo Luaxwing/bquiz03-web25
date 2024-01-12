78f07af77e3a34e03ef69491a5bfa5ad3a4899fa
Luaxwing <nick890111@gmail.com>
2024-01-12 09:45:25 +0800

    20240112/movie//show.sw.del

--- back/movie.php
+++ back/movie.php
@@ -25,65 +25,79 @@
 </style> -->
 
 <div style="width:100%;height:415px;overflow:auto">
-<?php
-$movies=$Movie->all(" order by rank");
-foreach($movies as $idx => $movie){
-?>
+    <?php
+    $movies = $Movie->all(" order by rank");
+    foreach ($movies as $idx => $movie) {
+        ?>
 
-<div class="item">
-    <div>
-        <img src="./img/<?=$movie['poster'];?>" style="width:100%">
-    </div>
-    <div>
-        分級:<img src='./icon/03C0<?=$movie['level'];?>.png' style='width:25px'>
-    </div>
-    <div>
-        <div style="display:flex;width:100%">
-            <div style="width:33.33%">
-                片名:<?=$movie['name'];?>
+        <div class="item">
+            <div>
+                <img src="./img/<?= $movie['poster']; ?>" style="width:100%">
             </div>
-            <div style="width:33.33%">
-                片長:<?=$movie['length'];?>
+            <div>
+                分級:<img src='./icon/03C0<?= $movie['level']; ?>.png' style='width:25px'>
             </div>
-            <div style="width:33.33%">
-                上映時間:<?=$movie['ondate'];?>
+            <div>
+                <div style="display:flex;width:100%">
+                    <div style="width:33.33%">
+                        片名:
+                        <?= $movie['name']; ?>
+                    </div>
+                    <div style="width:33.33%">
+                        片長:
+                        <?= $movie['length']; ?>
+                    </div>
+                    <div style="width:33.33%">
+                        上映時間:
+                        <?= $movie['ondate']; ?>
+                    </div>
+                </div>
+                <div>
+                    <button class='show-btn' data-id="<?= $movie['id']; ?>">
+                        <?= ($movie['sh'] == 1) ? '顯示' : '隱蔵'; ?>
+                    </button>
+                    <button class='sw-btn' data-id="<?= $movie['id']; ?>"
+                        data-sw="<?= ($idx !== 0) ? $movies[$idx - 1]['id'] : $movie['id']; ?>">往上</button>
+                    <button class='sw-btn' data-id="<?= $movie['id']; ?>"
+                        data-sw="<?= ((count($movies) - 1) != $idx) ? $movies[$idx + 1]['id'] : $movie['id']; ?>">往下</button>
+                    <button class="edit-btn" data-id="<?= $movie['id']; ?>">編輯電影</button>
+                    <button class="del-btn" data-id="<?= $movie['id']; ?>">刪除電影</button>
+                </div>
+                <div>
+                    劇情介紹:
+                    <?= $movie['intro']; ?>
+                </div>
             </div>
         </div>
-        <div>
-            <button class='show-btn' data-id="<?=$movie['id'];?>">
-                <?=($movie['sh']==1)?'顯示':'隱蔵';?>
-            </button>
-            <button class='sw-btn'  
-                  data-id="<?=$movie['id'];?>" 
-                  data-sw="<?=($idx!==0)?$movies[$idx-1]['id']:$movie['id'];?>">往上</button>
-            <button class='sw-btn' 
-                  data-id="<?=$movie['id'];?>" 
-                  data-sw="<?=((count($movies)-1)!=$idx)?$movies[$idx+1]['id']:$movie['id'];?>">往下</button>
-            <button class="edit-btn" data-id="<?=$movie['id'];?>">編輯電影</button>
-            <button class="del-btn" data-id="<?=$movie['id'];?>">刪除電影</button>
-        </div>
-        <div>
-            劇情介紹:<?=$movie['intro'];?>
-        </div>
-    </div>
-</div>
-<?php
-}
-?>
+        <?php
+    }
+    ?>
 </div>
 <script>
-$(".show-btn").on("click",function(){
-    
-})
-$(".sw-btn").on("click",function(){
+    $(".show-btn").on("click", function () {
+let id=$(this).data('id');
+$.post("./api/show.php", {id}, () => {
+            location.reload();
+        })
+    })
 
-})
-$(".edit-btn").on("click",function(){
+    $(".sw-btn").on("click", function () {
+        let id = $(this).data('id');
+        let sw = $(this).data('sw');
+        let table = 'movie';
+        $.post("./api/sw.php", { id, sw, table }, () => {
+            location.reload();
+        })
+    })
+    $(".edit-btn").on("click", function () {
 
-})
-$(".del-btn").on("click",function(){
-
-})
+    })
+    $(".del-btn").on("click", function () {
+        let id=$(this).data('id');
+        $.post("./api/del.php", { id,table:'movie'}, () => {
+            location.reload();
+        })
+    })
 
 
 </script>
