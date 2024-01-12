032a7812fd758ed69a4798b1802011c6af4ba3e7
Luaxwing <nick890111@gmail.com>
2024-01-12 16:17:50 +0800

    20240112/main//left_right//button

--- front/intro.php
+++ front/intro.php
@@ -1,7 +1,7 @@
-﻿﻿<?php $movie=$Movie->find($_GET['id']);?>
+﻿﻿﻿<?php $movie=$Movie->find($_GET['id']);?>
 <div class="tab rb" style="width:87%;">
   <div style="background:#FFF; width:100%; color:#333; text-align:left">
-    <video src="video/<?=$movie['trailer'];?>" width="300px" height="250px" controls="" style="float:right;"></video>
+    <video src="img/<?=$movie['trailer'];?>" width="300px" height="250px" controls="" style="float:right;"></video>
     <font style="font-size:24px">
         <img src="img/<?=$movie['poster'];?>" width="200px" height="250px" style="margin:10px; float:left">
       <p style="margin:3px">影片名稱 ：<?=$movie['name'];?>
@@ -18,14 +18,8 @@
       <?=$movie['intro'];?>
       </p>
     </font>
-    <table width="100%" border="0">
-      <tbody>
-        <tr>
-          <td align="center">
-            <input type="button" value="院線片清單" onclick="location.href='index.php'">
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="ct">
+        <button onclick="location.href='index.php'">院線片清單</button>
+    </div>
   </div>
 </div>
