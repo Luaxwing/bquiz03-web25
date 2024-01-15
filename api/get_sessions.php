a942c8cb015e8f4ecbaaa7a0bef66533d3442cb6
Luaxwing <nick890111@gmail.com>
2024-01-15 15:16:27 +0800

    20240115:增加判斷非當日的場次選擇

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -1,11 +1,29 @@
 <?php include_once 'db.php';
 $movie=$_GET['movie'];
 $date=$_GET['date'];
+$H=date("G");
+$start=($H>=14 && $date==date("Y-m-d"))?7-ceil(((24-$H)/2)):1;
 
-for($i=0;$i<5;$i++){
+for($i=$start;$i<=5;$i++){
     echo "<option value='{$sess[$i]}'>";
-    echo "{$sess[$i]}";
+    echo "{$sess[$i]} 剩餘座位 20";
     echo "</option>";
 }
+
+/***
+ * 1.去資料表撈出電影，日期場次訂單
+ * 2.根據訂單，機算座位數
+ * 3.在迴圈使用20-座位數來取得剩餘座位數
+ */
+
+
+// $movie=$_GET['movie'];
+// $date=$_GET['date'];
+// $H=date("G");
+// $start=($H>=14 && $date==date("Y-m-d"))?7-ceil((24-$H)/2):1;
+
+// for($i=$start;$i<=5;$i++){
+//     echo "<option value='{$sess[$i]}'>{$sess[$i]} 剩餘座位 20</option>";
+// }
 
 ?>
