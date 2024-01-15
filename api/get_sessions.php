8aac3bdc5629f5847dc49272f3be88f9051eb032
Luaxwing <nick890111@gmail.com>
2024-01-15 16:43:30 +0800

    20240115

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -1,6 +1,6 @@
 <?php include_once 'db.php';
 $movie=$_GET['movie'];
-$movieName=$Movie->find($movie)['movie'];
+$movieName=$Movie->find($movie)['name'];
 $date=$_GET['date'];
 $H=date("G");
 $start=($H>=14 && $date==date("Y-m-d"))?7-ceil(((24-$H)/2)):1;
@@ -17,7 +17,7 @@ $lave=20-$qt;
 
 
     echo "<option value='{$sess[$i]}'>";
-    echo "{$sess[$i]} 剩餘座位 20";
+    echo "{$sess[$i]} 剩餘座位 {$lave}";
     echo "</option>";
 }
 
